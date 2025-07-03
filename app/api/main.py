84bd590b69ba2a96943b93be986a674ddf38fa6f
kknaks <benesia93@naver.com>
2025-07-03 20:53:39 +0900

    .

--- app/api/main.py
+++ app/api/main.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from app.api.routes.v1 import auth
+from app.api.routes.v1 import auth, resume
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth")
+api_router.include_router(resume.router, prefix="/resume")
 
