600d02a8d181ea2f60bf7ed0368cb520751c8e47
JAWSP <lightload2912@gmail.com>
2025-07-07 14:38:58 +0900

    add jinro entitiy

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 from sqlmodel import SQLModel, Field, Relationship
 from sqlalchemy.sql import func
-from typing import List
+from typing import List, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .resume import Resume
+    from .jinro import Jinro
 
 class User(SQLModel, table=True):
     __tablename__ = "users"
@@ -25,3 +29,4 @@ class User(SQLModel, table=True):
     )
 
     resumes: List["Resume"] = Relationship(back_populates="user")
+    jinros: List["Jinro"] = Relationship(back_populates="user")
