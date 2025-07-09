0afdded7dcfe875c69370d454ff8eccb0351014b
2zm00 <ljm010301@naver.com>
2025-07-09 06:24:16 +0000

    fix: 불필요한 TokenPayload 임포트문 수정

--- app/api/deps.py
+++ app/api/deps.py
@@ -11,7 +11,7 @@ from sqlmodel import Session
 from app.core import security
 from app.core.config import settings
 from app.core.db import engine
-from app.models import TokenPayload, User
+from app.models import User
 from app.schemas.auth import UserInfo
 
 def get_db() -> Generator[Session, None, None]:
