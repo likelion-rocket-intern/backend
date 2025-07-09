4483b2d386bfde151a4ee4209f324b7be463da1b
2zm00 <ljm010301@naver.com>
2025-07-09 13:34:42 +0000

    fix: object_id 타입 변경에 따른 수정

--- app/repository/sql_embedding_repository.py
+++ app/repository/sql_embedding_repository.py
@@ -6,7 +6,7 @@ class SqlEmbeddingRepository:
     def __init__(self, db):
         self.db = db
 
-    def exists(self, object_id: int, object_type: str) -> bool:
+    def exists(self, object_id: str, object_type: str) -> bool:
         return bool(embedding_crud.get_by_object(self.db, object_id=object_id, object_type=object_type))
 
     def save(self, embedding: Embedding) -> None:
