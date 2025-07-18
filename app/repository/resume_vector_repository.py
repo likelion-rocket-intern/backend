b509edfa9ca2a68c20f0a7605de9acf56c0e1d13
2zm00 <ljm010301@naver.com>
2025-07-18 07:04:35 +0000

    feat: 이력서 벡터를 DB에서 가져오기위한 repository 계층 구현

--- app/repository/resume_vector_repository.py
+++ app/repository/resume_vector_repository.py
@@ -5,10 +5,9 @@ from sqlmodel import Session, select
 
 from app.core.config import settings
 from app.interface.resume_vector_repository import ResumeVectorRepositoryInterface
-from app.models.resume_embedding import ResumeEmbedding
-from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+from app.models.resume import ResumeEmbedding
+from langchain_core.embeddings import Embeddings
 from langchain_core.documents import Document
-from pgvector.sqlalchemy import cosine_distance
 
 logger = logging.getLogger(__name__)
 
@@ -18,13 +17,9 @@ class SqlResumeVectorRepository(ResumeVectorRepositoryInterface):
     SQLModel과 pgvector를 사용하여 이력서 벡터를 관리하는 저장소 구현체.
     """
 
-    def __init__(self, db: Session):
+    def __init__(self, db: Session, embeddings_model: Embeddings):
         self.db = db
-        self.embeddings = HuggingFaceInstructEmbeddings(
-            model_name="intfloat/multilingual-e5-large",
-            embed_instruction="query: ",
-            query_instruction="",
-        )
+        self.embeddings = embeddings_model
 
     def search_similar_chunks(
         self, resume_id: int, query: str, k: int = 5
@@ -37,9 +32,13 @@ class SqlResumeVectorRepository(ResumeVectorRepositoryInterface):
         statement = (
             select(ResumeEmbedding)
             .where(ResumeEmbedding.resume_id == resume_id)
-            .order_by(cosine_distance(ResumeEmbedding.embedding, query_vector))
+            .order_by(ResumeEmbedding.embedding.cosine_distance(query_vector))
             .limit(k)
         )
         results = self.db.exec(statement).all()
         
-        return [Document(page_content=res.chunk, metadata=res.metadata) for res in results]
+        return [Document(page_content=res.content,metadata={
+            'id': res.id, 
+            'resume_id': res.resume_id,
+            'chunk_index': res.chunk_index
+        }) for res in results]
